ac7a84cf4a1767b8433e25e47df2342a66bdb663
Alex Jordan <alex@strugee.net>
2017-01-24 14:21:01 -0500

    Use the cube theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,11 @@ var hash = require('bespoke-hash');
 var prism = require('bespoke-prism');
 var multimedia = require('bespoke-multimedia');
 var extern = require('bespoke-extern');
+var cube = require('bespoke-theme-cube');
 
 // Bespoke.js
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+	cube(),
 	classes(),
 	nav(),
 	scale(),
